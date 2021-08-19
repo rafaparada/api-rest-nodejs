181c489f6f8000953ec891d0d1177609f390935a
adalberto_parada <rafa.adalberto@gmail.com>
2021-08-19 16:12:07 -0600

    Variables en .env

--- config/conexion.js
+++ config/conexion.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql';
 
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+dotenv.config();
 const conexion = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'apirest_db'
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
 });
 
 conexion.connect((err)=>{
